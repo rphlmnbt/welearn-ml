0d560bf249c725bb7050ffa74125d3352acfe4e5
rphlmnbt <rphlmnbt@gmail.com>
2022-02-03 21:23:24 +0800

    Created method for new data to csv file.

--- services/mlService.py
+++ services/mlService.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import csv
 from sklearn.ensemble import RandomForestClassifier
 
 
@@ -17,3 +18,17 @@ class MLService:
         print(model.predict([stats]))
         predictions_proba = model.predict_proba([stats])
         return predictions_proba
+
+    def add(self, stats_arr, user_id, out):
+        q1 = stats_arr[0]
+        q2 = stats_arr[1]
+        q3 = stats_arr[2]
+        q4 = stats_arr[3]
+        q5 = stats_arr[4]
+        dir = os.path.dirname(__file__)
+        filename = os.path.join(dir, '../datasets/user-' + user_id + '.csv')
+        with open(filename, 'w', newline="", encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow([q1, q2, q3, q4, q5, out])
+        success_msg = "Data Added!"
+        return success_msg
